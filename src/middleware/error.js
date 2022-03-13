c445c7c534b029a21c7c86c159eddc2b0d8d41c0
edilsonlonc <eddylson.londonio@gmail.com>
2022-03-12 21:09:22 -0500

    Created logger

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,7 +1,7 @@
 const response = require('../response');
 
 // eslint-disable-next-line
-module.exports = (err, _, res, next) => {
+module.exports = (err, _, res, __) => {
   const { statusCode = 500 } = err;
   return response(res)({ ...err, statusCode });
 };
